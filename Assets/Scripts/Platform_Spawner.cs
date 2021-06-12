27bccb7cd23d40e8977e2afa39b788e1081bbd14
Megan Fuhr <MeganFuhr@gmail.com>
2021-06-12 07:47:11 -0400

    added cat_2. updated position of cat_2 for spawn

--- Assets/Scripts/Platform_Spawner.cs
+++ Assets/Scripts/Platform_Spawner.cs
@@ -8,12 +8,15 @@ public class Platform_Spawner : MonoBehaviour
     public int catToSpawn;
     public GameObject go;
     private Vector3 platformPosition;
+    [SerializeField]
+    private Vector3 cat_2;
 
     // Start is called before the first frame update
     void Start()
     {
         catToSpawn = Random.Range(1, 4);
         platformPosition = new Vector3(0.0f, -4.5f, 0.0f);
+        cat_2 = new Vector3(0, -2.78f, 0);
         CatRandomSpawn();
 
     }
@@ -29,7 +32,7 @@ public class Platform_Spawner : MonoBehaviour
                 go = Instantiate(cat1, this.gameObject.transform.localPosition = platformPosition, Quaternion.Euler(0, 0, 0));
                 break;
             case 2:
-                go = Instantiate(cat2, this.gameObject.transform.localPosition = platformPosition, Quaternion.Euler(0, 0, 0));
+                go = Instantiate(cat2, this.gameObject.transform.localPosition = cat_2, Quaternion.Euler(0, 0, 0));
                 break;
             case 3:
                 go = Instantiate(cat3, this.gameObject.transform.localPosition = platformPosition, Quaternion.Euler(0, 0, 0));
